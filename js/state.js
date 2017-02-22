47f0b9931fd218f2d1dbebb70d6382634ba4b9d8
Alex Buchanan <buchanae@ohsu.edu>
2017-02-22 15:30:54 -0800

    Generate all clients

---
+++ js/state.js
@@ -0,0 +1,24 @@
+/**
+ * @fileoverview
+ * @enhanceable
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+
+goog.provide('proto.ga4gh_task_exec.State');
+
+/**
+ * @enum {number}
+ */
+proto.ga4gh_task_exec.State = {
+  UNKNOWN: 0,
+  QUEUED: 1,
+  RUNNING: 2,
+  PAUSED: 3,
+  COMPLETE: 4,
+  ERROR: 5,
+  SYSTEMERROR: 6,
+  CANCELED: 7,
+  INITIALIZING: 8
+};
+
